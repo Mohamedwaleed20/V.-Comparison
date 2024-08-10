21b2c2dd9e8d1f7c8077fe054e79f4caeaaa97a9
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-10 22:41:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    char S;
+
+
+    cin >> A >> S >> B;
+
+
+    if (S == '<')
+    {
+        if (A < B)
+            cout << "Right" << endl;
+        else
+            cout << "Wrong" << endl;
+    }
+    else if (S == '>')
+    {
+        if (A > B)
+            cout << "Right" << endl;
+        else
+            cout << "Wrong" << endl;
+    }
+    else
+    { // S == '='
+        if (A == B)
+            cout << "Right" << endl;
+        else
+            cout << "Wrong" << endl;
+    }
+
+    return 0;
+}
