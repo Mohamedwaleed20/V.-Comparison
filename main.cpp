e16a2fccca445c775520a464be5f711974c9dda0
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-11-01 21:37:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a , b ;
+    char s ;
+    cin >> a >> s >> b ;
+    if(s=='>')
+    {
+        if(a>b)
+        {
+            cout << "Right";
+        }
+        else
+        {
+            cout << "Wrong";
+        }
+    }
+    if(s=='<')
+    {
+        if(a<b)
+        {
+            cout << "Right";
+        }
+        else
+        {
+            cout << "Wrong";
+        }
+    }
+    if(s=='=')
+    {
+        if(a==b)
+        {
+            cout << "Right";
+        }
+        else
+        {
+            cout << "Wrong";
+        }
+    }
+    return 0;
+}
